b9a5d62365fd428f2b18020699f1898cddc9e3c1
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-03-29 21:49:30 -0500

    fetching products

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,9 +1,20 @@
 import Stripe from "stripe";
-import '../../../envConfig.js'
+import "../../../envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY
+const API_KEY = process.env.STRIPE_SECRET_KEY;
 const stripe = new Stripe(API_KEY);
 
 export async function GET() {
-    
-} 
+  try {
+      // fetch all the active products from stripe
+
+      //fetch all the prices that are active
+
+      
+      //combine the products and their associated prices
+      
+  } catch (err) {
+    console.error("Error fetching data from stripe:", err.message);
+    return Response.json({ error: "Failed to fetch data from stripe" });
+  }
+}
