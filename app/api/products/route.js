b42a78da63243ff6a66757eeef506c5ee0f0f6c9
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-04-03 17:51:54 -0500

    google fonts

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,10 +1,10 @@
 import Stripe from "stripe";
 import "@/envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY;
-console.log("Stripe API Key loaded:", API_KEY ? "Yes" : "No");
-
-const stripe = new Stripe(API_KEY);
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const stripe = new Stripe(API_KEY, {
+  apiVersion: "2023-10-16",
+});
 
 export async function GET() {
   try {
