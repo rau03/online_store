604a6d8403c006a819c08101df7293a2d039ddd1
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-04-03 18:35:40 -0500

    fixed headjs file

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,7 +1,9 @@
 import Stripe from "stripe";
 import "@/envConfig.js";
 
-const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const API_KEY = process.env.STRIPE_SECRET_KEY;
+console.log("Stripe API Key loaded:", API_KEY ? "Yes" : "No");
+
 const stripe = new Stripe(API_KEY, {
   apiVersion: "2023-10-16",
 });
