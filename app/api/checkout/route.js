fa532c88f3eaed15e9b974f7c465e0379a1f1e27
Chris Rau <98765332+rau03@users.noreply.github.com>
2025-04-04 08:48:43 -0500

    api changes

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -1,10 +1,8 @@
 import Stripe from "stripe";
 import "@/envConfig.js";
 
-const API_KEY = process.env.STRIPE_SECRET_KEY;
-const stripe = new Stripe(API_KEY, {
-  apiVersion: "2023-10-16",
-});
+const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const stripe = new Stripe(API_KEY, { apiVersion: "2023-10-16" });
 
 export async function POST(request) {
   try {
